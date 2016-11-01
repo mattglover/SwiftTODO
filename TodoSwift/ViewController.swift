52ed77948c3aee99cadc7f940cf47e2cd320d5d1
Matt Glover <matt@duchysoftware.com>
2016-11-01 21:56:59 +0100

    Wired up FilePersistanceService (implementation of PersistanceService) and threw in some dummy data

--- TodoSwift/ViewController.swift
+++ TodoSwift/ViewController.swift
@@ -1,17 +1,26 @@
-//
-//  ViewController.swift
-//  TodoSwift
-//
-//  Created by Matt Glover on 01/11/2016.
-//  Copyright © 2016 Duchy Software. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+	var todoRepository: TodoRepository!
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
+
+		todoRepository = TodoRepository()
+		todoRepository.persistanceService = FilePersistanceService()
+
+//		let todo1 = Todo(name: "Todo 1", favorited: false, state: .NotDone)
+//		let todo2 = Todo(name: "Todo 2", favorited: false, state: .NotDone)
+//		let todo3 = Todo(name: "Todo 3", favorited: false, state: .NotDone)
+//		let todo4 = Todo(name: "Todo 4", favorited: false, state: .NotDone)
+//		let todo5 = Todo(name: "Todo 5", favorited: false, state: .NotDone)
+//		todoRepository.addTodo(todo: todo1)
+//		todoRepository.addTodo(todo: todo2)
+//		todoRepository.addTodo(todo: todo3)
+//		todoRepository.addTodo(todo: todo4)
+//		todoRepository.addTodo(todo: todo5)
 	}
 	
 }
