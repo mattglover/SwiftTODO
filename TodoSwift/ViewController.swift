02bb9af89aa22f2b620bd796e4687d953d6c9051
Matt Glover <matt@duchysoftware.com>
2016-11-06 17:35:11 +0100

    Removed unnecessary commented code

--- TodoSwift/ViewController.swift
+++ TodoSwift/ViewController.swift
@@ -42,19 +42,6 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
-//		todoRepository = TodoRepository()
-//		todoRepository.persistanceService = FilePersistanceService()
-
-//		let todo1 = Todo(name: "Todo 1", favorited: false, state: .NotDone)
-//		let todo2 = Todo(name: "Todo 2", favorited: false, state: .NotDone)
-//		let todo3 = Todo(name: "Todo 3", favorited: false, state: .NotDone)
-//		let todo4 = Todo(name: "Todo 4", favorited: false, state: .NotDone)
-//		let todo5 = Todo(name: "Todo 5", favorited: false, state: .NotDone)
-//		todoRepository.addTodo(todo: todo1)
-//		todoRepository.addTodo(todo: todo2)
-//		todoRepository.addTodo(todo: todo3)
-//		todoRepository.addTodo(todo: todo4)
-//		todoRepository.addTodo(todo: todo5)
 	}
 }
 
