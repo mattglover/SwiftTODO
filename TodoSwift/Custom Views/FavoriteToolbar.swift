7433eefb758f7f8136df1734b54d1ac514901330
Matt Glover <matt@duchysoftware.com>
2016-11-21 19:40:48 +0100

    Added basic implementation of FavoriteToolbar

--- TodoSwift/Custom Views/FavoriteToolbar.swift
+++ TodoSwift/Custom Views/FavoriteToolbar.swift
@@ -2,4 +2,30 @@ import UIKit
 
 class FavoriteToolbar: UIToolbar {
 
+	fileprivate lazy var segmentedControl : UISegmentedControl = {
+		let segmentedControl = UISegmentedControl(frame: .zero);
+		segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+		return segmentedControl
+	}()
+
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		commonInitialization()
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		commonInitialization()
+	}
 }
+
+extension FavoriteToolbar {
+	fileprivate func commonInitialization() {
+		translatesAutoresizingMaskIntoConstraints = false
+		addSubview(segmentedControl)
+
+		segmentedControl.insertSegment(withTitle: "All", at: 0, animated: false)
+		segmentedControl.insertSegment(withTitle: "Favorite", at: 1, animated: false)
+	}
+}
+
--- TodoSwiftTests/CustomView Tests/FavoriteToolbarTest.swift
+++ TodoSwiftTests/CustomView Tests/FavoriteToolbarTest.swift
@@ -4,4 +4,46 @@ import XCTest
 
 class FavoriteToolbarTest: XCTestCase {
 
+	var sut: FavoriteToolbar!
+
+	override func setUp() {
+		super.setUp()
+		sut = FavoriteToolbar(frame: .zero)
+
+	}
+
+	override func tearDown() {
+		sut = nil
+		super.tearDown()
+	}
+
+	func testWhenCreated_hasASegmentedControlSubview() {
+		var hasSegmentedControl = false
+		for subview in sut.subviews {
+			if subview.isKind(of: UISegmentedControl.self) {
+				hasSegmentedControl = true
+				break
+			}
+		}
+
+		XCTAssertTrue(hasSegmentedControl)
+	}
+
+	// MARK: SegmentedControl
+	func testSegmentedControl_hasHasTwoSegments() {
+		let segmentedControl = self.segmentedControl(fromToolbar: sut)
+
+		XCTAssertEqual(2, segmentedControl.numberOfSegments)
+	}
+
+	func segmentedControl(fromToolbar: FavoriteToolbar) -> UISegmentedControl {
+		var segmentedControl: UISegmentedControl!
+		for subview in fromToolbar.subviews {
+			if subview.isKind(of: UISegmentedControl.self) {
+				segmentedControl = subview as! UISegmentedControl
+				break
+			}
+		}
+		return segmentedControl
+	}
 }
